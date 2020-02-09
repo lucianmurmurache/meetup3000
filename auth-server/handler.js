9c7db0de7bee58b7296037076505ac7825650895
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-02-09 18:03:27 +0100

    Serverless error-events for getAccessToken must be array-working on fix!

---
+++ auth-server/handler.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const axios = require('axios');
+
+module.exports.getAccessToken = async (event) => {
+
+  const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access' +
+    '?client_id=he1k2l2636m6akrde4o968joim' +
+    '&client_secret=ve3sdie3hq9t2ipjgb87pptah5' +
+    '&grant_type=authorization_code' +
+    'redirect_uri=https://lucianmurmurache.github.io/' +
+    '&code=' + event.pathParameters.code;
+
+  const info = await axios.post(MEETUP_OAUTH_URL);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
+  };
+};
