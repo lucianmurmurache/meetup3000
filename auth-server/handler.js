57bf5389bbfa83e9dac9df4d6542c3bf46cecb54
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-02-13 21:32:56 +0100

    Added refreshAccessToken function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -24,3 +24,25 @@ module.exports.getAccessToken = async (event) => {
     }),
   };
 };
+
+module.exports.refreshAccessToken = async (event) => {
+
+  const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access' +
+    '?client_id=he1k2l2636m6akrde4o968joim' +
+    '&client_secret=ve3sdie3hq9t2ipjgb87pptah5' +
+    '&grant_type=refresh_token' +
+    '&refresh_token=' + event.pathParameters.refresh_token;
+
+  const info = await axios.post(MEETUP_OAUTH_URL);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
+  };
+};
