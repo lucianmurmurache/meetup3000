97e096a180c63bbe3a4c3f80f70ddb74d23dc43d
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-02-15 21:23:16 +0100

    Task-4-3 - Unit testing

---
+++ src/Event.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Event extends Component {
+    state = {
+        event: {},
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default Event;
