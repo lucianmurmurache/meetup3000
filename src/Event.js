fcc04c260f3fdacfdb72433e2a67268131e7b3c2
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-02-29 16:29:23 +0100

    Updated Event styling

--- src/Event.js
+++ src/Event.js
@@ -22,16 +22,16 @@ class Event extends Component {
             <div className="event">
                 <div className="event-overview">
                     <p className="event-overview-name">{this.props.event.name}</p>
-                    <p className="event-overview-date"><span className="label">When: </span>{this.props.event.local_date}</p>
-                    <p className="event-overview-time"><span className="label">Time: </span>{this.props.event.local_time}</p>
+                    <p className="event-overview-date"><span>Event date: </span>{this.props.event.local_date}</p>
+                    <p className="event-overview-time"><span>Event time: </span>{this.props.event.local_time}</p>
                     <button className="event-details-btn" onClick={() => this.handleShowDetails()}>Show more</button>
                 </div>
                 {showDetails &&
                     <div className="event-details">
-                        <p className="event-venue"><span className="label">Venue name: </span>{this.props.event.venue && this.props.event.venue.name}</p>
-                        <p className="event-overview-max"><span className="label">Max capacity: </span>{this.props.event.rsvp_limit}</p>
-                        <p className="event-overview-going"><span className="label">Going: </span>{this.props.event.yes_rsvp_count}</p>
-                        <p className="event-overview-waitlist"><span className="label">Waitlist: </span>{this.props.event.waitlist_count}</p>
+                        <p className="event-details-venue"><span>Venue name: </span>{this.props.event.venue && this.props.event.venue.name}</p>
+                        <p className="event-details-max"><span>Max capacity: </span>{this.props.event.rsvp_limit}</p>
+                        <p className="event-details-going"><span>Going: </span>{this.props.event.yes_rsvp_count}</p>
+                        <p className="event-details-waitlist"><span>Waitlist: </span>{this.props.event.waitlist_count}</p>
                         <p className="event-details-description">{this.props.event.description}</p>
                     </div>
                 }
